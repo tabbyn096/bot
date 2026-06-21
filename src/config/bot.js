fb64e3e7a475394a651d02f77ebd156f93f8f37c
tabbyn096 <contact.stannmc@gmail.com>
2026-06-21 20:16:31 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Managing the Nexus community",
+        name: "https://discord.gg/X8wGnJ3pFT",
         // Activity type number (0 = Playing).
         type: 4,
       },
